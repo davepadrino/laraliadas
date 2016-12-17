e215b340bb7396fe5e5a22f9da929c9e034df226
Miguel F <miguelachof11@gmail.com>
2016-12-17 11:49:50 -0400

    cambios a la vista recover

--- resources/views/recover_password.blade.php
+++ resources/views/recover_password.blade.php
@@ -7,24 +7,22 @@
 			</div>
 			<div>
 				{!! Form::open(['url' => '/password/email'])!!}
-						<table id="form">
-							<tr>
-								<td>
-									{!! Form::email('email',null,['class'=>'form-control', 'placeholder'=>'Escriba cuenta de correo electronico asociada'])!!}
-								</td>
-							</tr>
-							<tr>
-								<td>
-									{!! Form::submit('Enviar', ['class'=>'btn btn-primary'])!!}
-								</td>
-							</tr>
-							<tr>
-								<td>
-									{!! link_to('/', "Volver", $attributes = ['id' => 'rec_pass', 'class'=> 'btn btn-danger', 'style' => 'color:white']) !!}
-								</td>
-							</tr>
-						</table>
-						<span>Se le enviará un correo con un enlace para recuperar su contraseña</span>
+					<table id="form">
+						<tr>
+							<td>
+								{!! Form::email('email',null,['class'=>'form-control', 'placeholder'=>'Escriba cuenta de correo electronico asociada'])!!}
+							</td>
+						</tr>
+						<tr>
+							<td>
+								{!! Form::submit('Enviar', ['class'=>'btn btn-primary'])!!}
+							</td>
+							<td>
+								{!! link_to('/', "Volver", $attributes = ['id' => 'rec_pass', 'class'=> 'btn btn-danger', 'style' => 'color:white']) !!}
+							</td>
+						</tr>
+					</table>
+					<span>Se le enviará un correo con un enlace para recuperar su contraseña</span>
 				{!! Form::close() !!}			
 			</div>
 		</div>
